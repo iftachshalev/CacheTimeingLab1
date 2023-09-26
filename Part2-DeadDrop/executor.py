abd2f04afca3d93fe5873ceac90b06a545e58dd0
Iftach Shalev <62513436+iftachshalev@users.noreply.github.com>
2023-09-26 20:27:57 +0300

    Delete Part2-DeadDrop directory

--- Part2-DeadDrop/executor.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-core_number = 4
-receiver_path = "/home/iftach/Documents/CacheTimeingLab1/Part2-DeadDrop/receiver"
-sender_path = "/home/iftach/Documents/CacheTimeingLab1/Part2-DeadDrop/sender"
-
-os.system(f"gnome-terminal -- bash -c 'taskset -c {core_number} {receiver_path}; read -p \"Press Enter to close...\"'")
-os.system(f"gnome-terminal -- bash -c 'taskset -c {core_number} {sender_path}; read -p \"Press Enter to close...\"'")
